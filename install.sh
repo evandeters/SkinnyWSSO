a69565d84e86f90142c4929957e5475dd3a70de2
evanjd711 <evanjd711@gmail.com>
2023-12-07 11:14:59 -0800

    underscores

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ cd /opt/skinnywsso/tls
 openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 3650 -nodes -subj "/C=US/ST=CA/L=Pomona/O=SkinnyWSSO/OU=SkinnyWSSO/CN=skinny.wsso"
 docker-compose up -d
 sleep 3
-docker exec skinnywsso-ldap-1 ldapadd -Y EXTERNAL -H ldapi:/// -f /opt/skinny_wsso/wsso.ldif
+docker exec skinnywsso_ldap_1 ldapadd -Y EXTERNAL -H ldapi:/// -f /opt/skinny_wsso/wsso.ldif
