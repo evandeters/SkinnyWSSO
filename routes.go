a6e0632b48fcad083a3f70caea603a2d2f10292e
evanjd711 <evanjd711@gmail.com>
2023-12-07 13:17:21 -0800

    api changes

--- routes.go
+++ routes.go
@@ -16,20 +16,20 @@ func addPublicRoutes(g *gin.RouterGroup) {
 	g.GET("/register", func(c *gin.Context) {
 		c.HTML(200, "register.html", gin.H{})
 	})
-	g.POST("/login", login)
-	g.POST("/register", register)
-	g.GET("/health", health)
+	g.POST("/api/users/login", login)
+	g.POST("/api/users/register", register)
+	g.GET("/api/status", status)
 }
 
 func addPrivateRoutes(g *gin.RouterGroup) {
 	g.GET("/logout", logout)
-	g.GET("/admin", gin.BasicAuth(gin.Accounts{"admin": os.Getenv("WSSO_ADMIN_PASSWORD")}), func(c *gin.Context) {
+	g.GET("/admin", gin.BasicAuth(gin.Accounts{os.Getenv("WSSO_ADMIN_USERNAME"): os.Getenv("WSSO_ADMIN_PASSWORD")}), func(c *gin.Context) {
 		c.HTML(200, "admin.html", gin.H{})
 	})
 	g.GET("/api/users/list", listUsers)
 	g.DELETE("/api/users/delete/:username", deleteUser)
 }
 
-func health(c *gin.Context) {
+func status(c *gin.Context) {
 	c.JSON(200, gin.H{"status": "ok"})
 }
