aeec9bba5f6c9354815cb3d38f52ac288ff3efb8
Evan Deters <evanjd711@gmail.com>
2023-12-07 16:08:37 -0800

    no -d

--- install.sh
+++ install.sh
@@ -15,6 +15,6 @@ cd /opt/skinnywsso/tls
 openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 3650 -nodes -subj "/C=US/ST=CA/L=Pomona/O=SkinnyWSSO/OU=SkinnyWSSO/CN=skinny.wsso"
 openssl genrsa -out $jwtprivatekey 2048
 openssl rsa -in $jwtprivatekey -pubout -out $jwtpublickey
-docker-compose up -d
+docker-compose up 
 sleep 3
 docker exec skinnywsso_ldap_1 ldapadd -Y EXTERNAL -H ldapi:/// -f /opt/skinny_wsso/wsso.ldif
